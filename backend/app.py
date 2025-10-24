25274039d26f83e102536cb6aa02f9d9c7f97ecb
Swaathi <swaathi1409@gmail.com>
2025-10-24 10:27:39 +0530

    Update app.py - Fix CORS issue

--- backend/app.py
+++ backend/app.py
@@ -31,8 +31,10 @@ contacts_collection = db["contacts"]
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://pay-shield.vercel.app",
-    "http://localhost:5173"],
+    allow_origins=[
+        "https://pay-shield.vercel.app",
+        "http://localhost:5173"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
